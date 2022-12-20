327d5a72873ddcf4b690b94ad5517e780f37a99a
PabloEscobar1337 <giorgipapa8@gmail.com>
2022-12-20 12:59:48 -0800

    implemented arp

---
+++ src/arp.c
@@ -0,0 +1,36 @@
+#include "arp.h"
+
+void wc_arp_parse(const unsigned char *in, wc_arp *out) {
+    wc_arphdr *arp = (wc_arphdr*)(in + sizeof(wc_ethdr));
+    // hardware type
+    if (arp->ar_hrd == ARPHRD_ETHER)
+        out->hw_t = "ETHERNET";
+    else
+        out->hw_t = "OTHER";
+    // protocol type
+    out->p_t = wc_format("%d", arp->ar_pro);
+    // hardware len
+    out->hw_len = (uint8_t) arp->ar_hln;
+    // protocol len
+    out->p_len = (uint8_t) arp->ar_pln;
+    // arp opcode
+    switch (arp->ar_op) {
+        case ARPOP_REQUEST:
+            out->opcode = "REQUEST";
+        case ARPOP_REPLY:
+            out->opcode = "REPLY";
+        case ARPOP_NAK:
+            out->opcode = "NAK";
+        default:
+            out->opcode = "OTHER";
+    }
+    // sender mac
+    out->sender_mac = wc_format("%.2X-%.2X-%.2X-%.2X-%.2X-%.2X", arp->__ar_sha[0], arp->__ar_sha[1], arp->__ar_sha[2], arp->__ar_sha[3], arp->__ar_sha[4], arp->__ar_sha[5]);
+    // sender ip
+    out->sender_ip = wc_format("%.2X.%.2X.%.2X.%.2X", arp->__ar_sip[0], arp->__ar_sip[1], arp->__ar_sip[2], arp->__ar_sip[3]);
+    // target mac
+    out->target_mac = wc_format("%.2X-%.2X-%.2X-%.2X-%.2X-%.2X", arp->__ar_tha[0], arp->__ar_tha[1], arp->__ar_tha[2], arp->__ar_tha[3], arp->__ar_tha[4], arp->__ar_tha[5]);
+    // target ip
+    out->target_ip = wc_format("%.2X.%.2X.%.2X.%.2X", arp->__ar_tip[0], arp->__ar_tip[1], arp->__ar_tip[2], arp->__ar_tip[3]);
+}
+
