465b79093be5587040ba01af2856d98f4234f4fb
nikolaycc <lukaggt2003@gmail.com>
2023-01-05 17:01:22 +0400

    Added wrc_args(..)

--- src/wrc_main.c
+++ src/wrc_main.c
@@ -2,9 +2,11 @@
 
 // void custom_func(wc_pa pa) {
 //     fprintf(fp, "\n%s\n%s", pa.eth.source, pa.eth.dest);
-// } 
+// }
 
-int main(void) {
+void wrc_args(int argc, char** argv);
+
+int main(int argc, char** argv) {
     wrc cap;
     wc_iflist ifc = wrc_get_interfaces();
     
@@ -18,6 +20,15 @@ int main(void) {
     //               custom_func
     wrc_cap(&cap, 1, DEFAULT_CAP);  
     wrc_destroy(&cap);
-        
+    
     return 0; 
+}
+
+void wrc_args(int argc, char **argv) {
+  for (int i = 0; i < argc; i++) {
+    if (strcmp(argv[i], "-h") || strcmp(argv[i], "--help")) {
+        fprintf(stdout, "wirecroc\t [-if <INTEFACE> -c <FILE/STDOUT>]\n");
+        exit(0);
+    }
+  }
 }
