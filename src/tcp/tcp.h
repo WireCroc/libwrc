3caae61da8761a00999bda635c4956a6974fd45d
PabloEscobar1337 <giorgipapa8@gmail.com>
2023-01-05 17:40:27 +0400

    added a struct for tcp

--- src/tcp/tcp.h
+++ src/tcp/tcp.h
@@ -6,8 +6,18 @@
 #include "../utils/utils.h"
 #include <netinet/tcp.h>
 
-typedef struct tcphdr wc_tcp;
+typedef struct tcphdr wc_tcphdr;
+
+typedef struct {
+    uint16_t source;
+    uint16_t dest;
+    uint32_t sequence;
+    uint64_t ack_sequence;
+    uint16_t window;
+    uint16_t checksum;
+    uint8_t flag;
+} wc_tcp;
 
 void wc_tcp_p(const unsigned char*, wc_tcp*, uint16_t);
 
-#endif 
+#endif
